717ac8e3fa676b2c13e7a6aad02a4921be79420d
palloncino <antonio.guiotto.dev@gmail.com>
2021-10-19 17:47:45 +0200

    posts pt. 3

--- client/src/features/pages/posts/view.tsx
+++ client/src/features/pages/posts/view.tsx
@@ -1,25 +1,33 @@
 import { Button } from '../../button';
 import { IPostsProps } from './posts-interfaces';
 
-export const PostsContent = ({handleInputChange, handlePostPost, htmlInputValue, postsData}: IPostsProps) => {
+export const PostsContent = ({
+  handleInputChange,
+  handlePostPost,
+  htmlInputValue,
+  postsData,
+}: IPostsProps) => {
   return (
-    <div className="posts-container">
-      <textarea
-        className="posts-editable-html"
-        placeholder="write HTML code"
-        onChange={e => handleInputChange(e.target.value)}
-        id="posts-editable-html-input"
-        value={htmlInputValue}
-      />
-      <Button handleClick={handlePostPost} label="⚡️ SAVE"/>
+    <div className="posts-wrapper">
+      <div className="posts-form-container">
+        <textarea
+          className="posts-editable-html"
+          placeholder="write HTML code"
+          onChange={(e) => handleInputChange(e.target.value)}
+          id="posts-editable-html-input"
+          value={htmlInputValue}
+        />
+        <Button handleClick={handlePostPost} label="⚡️ SAVE" />
+      </div>
       <div className="posts-output-container">
         {postsData.map((post: any) => {
           return (
             <div className="post-container">
-              {post.id}
               <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+              <Button handleClick={() => {}} label="❌ DELETE POST" />
+              <Button disabled handleClick={() => {}} label="❌ EDIT POST" />
             </div>
-          )
+          );
         })}
       </div>
     </div>
