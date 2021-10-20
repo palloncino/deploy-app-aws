84d4404e75df9b52118773df752b7ecaf9bd4aeb
palloncino <antonio.guiotto.dev@gmail.com>
2021-10-20 12:46:14 +0200

    generic

--- client/src/features/pages/homepage/index.tsx
+++ client/src/features/pages/homepage/index.tsx
@@ -11,14 +11,54 @@ export function Homepage() {
 
   const renderInsider = () => {
     return (
-      <div className="homepage-text-container">
-        <div className="homepage-text-container-title">Welcome {name}</div>
-        <div>
-          <h3>
-            ⚠️ Work in progress, some features might be out of service.
-          </h3>
+      <div className="homepage-container">
+        <div className="homepage-group homepage-group--1">
+          <div className="homepage-text-container-title--XXXL">
+            Welcome {name}
+          </div>
+          <div>
+            <h3>⚠️ Work in progress, some features might be out of service.</h3>
+          </div>
         </div>
-        <Carousel />
+
+        <div className="homepage-group homepage-group--2">
+          <div className="homepage-text-container-title--XXL">
+            Latest posts
+          </div>
+          <Carousel />
+        </div>
+
+        <div className="homepage-group homepage-group--3">
+          <div className="homepage-text-container-title--XXL">
+            Take a look around
+          </div>
+          <div className="homepage-big-buttons">
+            <div className="homepage-big-button-container">
+              <div className="homepage-big-button-container-title">
+                <h3>
+                Manage your expenses
+                </h3>
+              </div>
+              <div className="homepage-big-button-container-image">💸</div>
+            </div>
+            <div className="homepage-big-button-container">
+              <div className="homepage-big-button-container-title">
+                <h3>
+                Visited countries
+                </h3>
+              </div>
+              <div className="homepage-big-button-container-image">📍</div>
+            </div>
+            <div className="homepage-big-button-container">
+              <div className="homepage-big-button-container-title">
+                <h3>
+                A. G. Artworks
+                </h3>
+              </div>
+              <div className="homepage-big-button-container-image">🎨</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
@@ -32,7 +72,7 @@ export function Homepage() {
   };
 
   return (
-    <div className="homepage-container">
+    <div className="homepage-wrapper">
       {isAuthenticated ? renderInsider() : renderOutsider()}
     </div>
   );
