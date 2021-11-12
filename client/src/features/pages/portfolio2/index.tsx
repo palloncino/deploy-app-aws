0198c2bf9e1d968c15d4f7a079537f8164ff0b3e
palloncino <antonio.guiotto.dev@gmail.com>
2021-11-12 13:13:51 +0100

    profile2

--- client/src/features/pages/portfolio2/index.tsx
+++ client/src/features/pages/portfolio2/index.tsx
@@ -21,13 +21,33 @@ export function Portfolio2() {
         </div>
         <div className="portfolio2__group portfolio2__group--2">
           <div className="portfolio2__group portfolio2__group--2__title">
-            Antonio Guiotto
+            Introduction to the man
           </div>
           <div className="portfolio2__group portfolio2__group--2__subtitle">
-            Web Developer
+            Who am I
           </div>
           <div className="portfolio2__group portfolio2__group--2__description">
-            It's a small blurred pic but is a happy face!
+            I am an Italian man, but I love to talk in english and explore new cultures.
+          </div>
+
+          <div className="portfolio2__group portfolio2__group--2__subtitle">
+            About me
+          </div>
+          <div className="portfolio2__group portfolio2__group--2__description">
+            Very active and sporty guy, in my youth I've got the chance to develop as a musician and as a visual artist.
+            Developing application is one of many things that I like to do.
+            I have a calm temper and I like to focus on my work.
+          </div>
+
+          <div className="portfolio2__group portfolio2__group--2__subtitle">
+            My job
+          </div>
+          <div className="portfolio2__group portfolio2__group--2__description">
+            Since 2018 I am working as a Web Developer, I take care of all sides of web applications, meaning #front-end #back-end #dev-ops #project-managment #business-logic #graphic-design #content-manager #creator
+          </div>
+
+          <div className="portfolio2__group portfolio2__group--2__subtitle">
+            Downloads
           </div>
           <div className="portfolio2__group portfolio2__group--2__button__container">
             <Button handleClick={() => handleDownloadFile('pdf')} label="Download CV" className="portfolio2__group portfolio2__group--2__button-container__btn" />
